b67bff425c5c2149630424c1473ac3cea5a70517
Robert W <1263192+rbw@users.noreply.github.com>
2024-03-19 01:26:13 +0100

    Remove redundant window setters

--- src/JottyWindow.cpp
+++ src/JottyWindow.cpp
@@ -8,14 +8,10 @@ JottyWindow::JottyWindow() {
     editor = new JottyEditor(this);
     setCentralWidget(editor);
 
-    // Set up timer for autosaving
+    // Set up timer for auto save
     QTimer *timer = new QTimer(this);
     connect(timer, &QTimer::timeout, editor, &JottyEditor::saveState);
     timer->start(2000);
-
-    // Set window properties
-    setWindowTitle("Jotty: Scribble Today, Remember Tomorrow");
-    setGeometry(100, 100, 600, 400);
 }
 
 void JottyWindow::closeEvent(QCloseEvent *event) {
